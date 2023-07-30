8c6c4e063282ae3b517431fb0a87181cc88db0c6
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2023-07-29 22:37:53 -0300

    Optimize code

--- Consolidar_RG17.py
+++ Consolidar_RG17.py
@@ -37,29 +37,25 @@ def Procesar_TXT_Consolidados():
     # Procesar archivo
 
     CertificadosIVA = CertificadosIVA[CertificadosIVA["% No ret"] == 100]
-    CertificadosIVA = CertificadosIVA.drop_duplicates()
+    CertificadosIVA = CertificadosIVA.drop_duplicates(subset=["CUIT" , "Desde RG17" , "Hasta RG17" , "% No ret"])
     CertificadosIVA["CUIT"] = CertificadosIVA['CUIT'].astype(np.int64)
     CertificadosIVA["Motivo RG17"] = "2" 
     CertificadosIVA["% No ret"] = CertificadosIVA["% No ret"].astype(int)
     CertificadosIVA["Desde Periodo"] = pd.to_datetime(CertificadosIVA["Desde RG17"] , format="%d/%m/%Y").dt.date.astype(str).str[0:7].str.replace("-" , "").astype(int)
     CertificadosIVA["Hasta Periodo"] = pd.to_datetime(CertificadosIVA["Hasta RG17"] , format="%d/%m/%Y").dt.date.astype(str).str[0:7].str.replace("-" , "").astype(int)
-    CertificadosIVA = CertificadosIVA.sort_values(by=["Hasta Periodo"] , ascending=False )
-    CertificadosIVA = CertificadosIVA.sort_values(by=["CUIT"] , ascending=True )
+    CertificadosIVA = CertificadosIVA.sort_values(by=["CUIT","Hasta Periodo",'Desde Periodo'], ascending=[True , False, True])
 
     # Resetear indice
     CertificadosIVA = CertificadosIVA.reset_index(drop=True)
+
+    # Eliminar los duplicados por 'CUIT' y 'Desde Periodo' y quedarse con el último
+    CertificadosIVA = CertificadosIVA.drop_duplicates(subset=['CUIT' , 'Desde Periodo'] , keep='last')
 
     # Crear columna con el numero actual de veces que se repite el CUIT si el indice es menor al actual
     CertificadosIVA['Ajuste'] = CertificadosIVA['CUIT'].astype(str) + ' - ' + (CertificadosIVA.groupby('CUIT').cumcount() + 1).astype(str)
 
     # Ordenar las columnas en el siguiente orden 'Ajuste' , 'CUIT' , 'Denominación' , 'Desde RG17' , 'Hasta RG17' , '% No ret' , 'Motivo RG17' , 'Desde Periodo' , 'Hasta Periodo'
     CertificadosIVA = CertificadosIVA[['Ajuste' , 'CUIT' , 'Denominación' , 'Desde RG17' , 'Hasta RG17' , '% No ret' , 'Motivo RG17' , 'Desde Periodo' , 'Hasta Periodo']]
-
-    # Reiniciar el indice
-    CertificadosIVA = CertificadosIVA.reset_index(drop=True)
-
-    # Eliminar los duplicados por 'CUIT', 'Denominación' y 'Desde Periodo' y quedarse con el primero
-    CertificadosIVA = CertificadosIVA.drop_duplicates(subset=['CUIT', 'Denominación' , 'Desde Periodo'] , keep='first')
 
     #Exportar archivo
     CertificadosIVA.to_csv("Consolidado procesado.csv" , index=False , encoding="latin1" , sep=";" )
